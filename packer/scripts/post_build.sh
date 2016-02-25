88e5251e7c938bda80386995bbcfd531fe66f861
Scott Overholser <eigenholser@users.noreply.github.com>
2016-02-24 21:02:23 -0700

    Fix problem with installing packages resulting in error message "No installation candidate."
    Solution:
    http://serverfault.com/questions/533011/build-essential-no-installation-candidate-sometimes

--- packer/scripts/post_build.sh
+++ packer/scripts/post_build.sh
@@ -1,8 +1,12 @@
 
 echo "Rebasing system."
 apt-get clean
+apt-get dist-upgrade
 apt-get update
+apt-get upgrade --assume-yes --fix-broken
+
 
 echo "Installing build tools."
 apt-get install --assume-yes \
-    build-essential curl gcc libc6-dev zlib1g-dev libffi-dev libssl-dev
+    build-essential curl gcc zlib1g-dev libffi-dev libssl-dev
+
