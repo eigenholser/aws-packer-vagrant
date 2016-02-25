893ea550871a2073055889ee97b0ff2b669ab58c
Scott Overholser <eigenholser@users.noreply.github.com>
2016-02-24 21:03:20 -0700

    Fix typo, fix problem.

--- packer/scripts/python_deps.sh
+++ packer/scripts/python_deps.sh
@@ -1,7 +1,7 @@
 
 # Install latest pip. Will also grab setuptools.
 echo "Installing pip."
-wget https://bootstrap.pypa.io/git-pip.py -O - | python
+wget https://bootstrap.pypa.io/get-pip.py -O - | python
 
 echo "Installing deps for ssl and urllib3."
 pip install pyopenssl ndg-httpsclient pyasn1
